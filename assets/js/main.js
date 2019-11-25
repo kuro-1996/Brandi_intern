62fbd5bf6d33c517a14e75c9b3ebb5ebd1fbc133
tuanmadlife1996 <ng.ngocanhtuan96@gmail.com>
2019-11-25 14:37:06 +0700

    update code ( add transition in header when scroll )

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,31 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 });
+
+$(function () {
+    var banner = $('.banner');
+    var header = $('header');
+    var anchor = $('.nav-bar_menu').children();
+    pos = banner.offset();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > pos.top + banner.height() - 70) {
+            $(header).addClass('change');
+            $(anchor).hover(function(){
+                $(this).css('color','#062033');
+            },
+            function () {
+                $(this).css('color', '#ffffff');
+            });
+        } else if ($(this).scrollTop() <= pos.top + banner.height() - 70 && header.hasClass('change')) {
+            $(header).removeClass('change');
+            $(anchor).hover(function(){
+                $(this).css('color','#32b0ee');
+            },
+            function () {
+                $(this).css('color', '#ffffff');
+
+            });
+        }
+    });
+});
