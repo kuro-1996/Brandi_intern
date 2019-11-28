1e70c54937a799bfa56991c22e1d701f74b42097
tuanmadlife1996 <ng.ngocanhtuan96@gmail.com>
2019-11-28 16:09:26 +0700

    update code ( complete responsive )

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $("a").click(function () {
-        $("a").removeClass("active");
+    $('.nav-bar_menu').children().click(function () {
+        $('.nav-bar_menu').children().removeClass("active");
         $(this).addClass("active");
     });
 });
@@ -10,31 +10,43 @@ $(function () {
     var header = $('header');
     var anchor = $('.nav-bar_menu').children();
     var para = $('.nav-bar_brand p');
-        pos = banner.offset();
+    pos = banner.offset();
 
     $(window).scroll(function () {
-        if ($(this).scrollTop() > pos.top + banner.height() - 70) {
+        if ($(this).scrollTop() > pos.top + banner.height() - header.height()) {
             $(header).addClass('change');
-            $(para).css('opacity','1');
+            $(para).css('opacity', '1');
             $(para).addClass('fix');
             $(anchor).addClass('fix');
-            $(anchor).hover(function(){
-                $(this).css('color','#062033');
+            $(anchor).hover(function () {
+                $(this).css('color', '#1B4F72 ');
             },
-            function () {
-                $(this).css('color', '#ffffff');
-            });
-        } else if ($(this).scrollTop() <= pos.top + banner.height() - 70 && header.hasClass('change')) {
+                function () {
+                    $(this).css('color', '#ffffff');
+                });
+        } else if ($(this).scrollTop() <= pos.top + banner.height() - header.height() && header.hasClass('change')) {
             $(header).removeClass('change');
             $(para).removeClass('fix');
             $(anchor).removeClass('fix');
-            $(anchor).hover(function(){
-                $(this).css('color','#32b0ee');
+            $(anchor).hover(function () {
+                $(this).css('color', '#32b0ee');
             },
-            function () {
-                $(this).css('color', '#ffffff');
+                function () {
+                    $(this).css('color', '#ffffff');
 
-            });
+                });
         }
     });
 });
+
+$('.nav-bar_btn').click(function () {
+    $(this).toggleClass('active');
+    $('.nav-bar_menu').toggleClass('active');
+    $('.btn_icon').toggleClass('active');
+});
+$('.nav-bar_menu').children().click(function () {
+    $('.nav-bar_btn').removeClass('active');
+    $('.nav-bar_menu').removeClass('active');
+    $('.btn_icon').removeClass('active');
+});
+
