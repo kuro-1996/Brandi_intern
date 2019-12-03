9be70c63dcecdd1907177c2370ce3f45ccf3dedf
tuanmadlife1996 <ng.ngocanhtuan96@gmail.com>
2019-12-03 13:34:38 +0700

    update code (fix bug)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,74 +3,77 @@ $(document).ready(function () {
         $('.nav-bar_menu').children().removeClass("active");
         $(this).addClass("active");
     });
-});
 
-$(document).ready(function () {
+
+
     $('.slick-wrap').slick({
         dots: true,
         loop: true,
-        arrow: false,
+        prevArrow: null,
+        nextArrow: null,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1
     });
-});
 
-$(document).ready(function () {
+
+
     $('.features-content').slick({
         dots: true,
         loop: true,
         infinite: true,
-        arrow: false,
+        prevArrow: null,
+        nextArrow: null,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1
     });
-});
 
-$(function () {
-    var banner = $('.banner');
-    var header = $('header');
-    var anchor = $('.nav-bar_menu').children();
-    var para = $('.nav-bar_brand p');
-    pos = banner.offset();
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > pos.top + banner.height() - header.height()) {
-            $(header).addClass('change');
-            $(para).css('opacity', '1');
-            $(para).addClass('fix');
-            $(anchor).addClass('fix');
-            $(anchor).hover(function () {
-                $(this).css('color', '#1B4F72 ');
-            },
-                function () {
-                    $(this).css('color', '#ffffff');
-                });
-        } else if ($(this).scrollTop() <= pos.top + banner.height() - header.height() && header.hasClass('change')) {
-            $(header).removeClass('change');
-            $(para).removeClass('fix');
-            $(anchor).removeClass('fix');
-            $(anchor).hover(function () {
-                $(this).css('color', '#32b0ee');
-            },
-                function () {
-                    $(this).css('color', '#ffffff');
+    $(function () {
+        var banner = $('.banner');
+        var header = $('header');
+        var anchor = $('.nav-bar_menu').children();
+        var para = $('.nav-bar_brand p');
+        pos = banner.offset();
 
-                });
-        }
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > pos.top + banner.height() - header.height()) {
+                $(header).addClass('change');
+                $(para).css('opacity', '1');
+                $(para).addClass('fix');
+                $(anchor).addClass('fix');
+                $(anchor).hover(function () {
+                    $(this).css('color', '#1B4F72 ');
+                },
+                    function () {
+                        $(this).css('color', '#ffffff');
+                    });
+            } else if ($(this).scrollTop() <= pos.top + banner.height() - header.height() && header.hasClass('change')) {
+                $(header).removeClass('change');
+                $(para).removeClass('fix');
+                $(anchor).removeClass('fix');
+                $(anchor).hover(function () {
+                    $(this).css('color', '#32b0ee');
+                },
+                    function () {
+                        $(this).css('color', '#ffffff');
+
+                    });
+            }
+        });
     });
-});
 
-$('.nav-bar_btn').click(function () {
-    $(this).toggleClass('active');
-    $('.nav-bar_menu').toggleClass('active');
-    $('.btn_icon').toggleClass('active');
-});
-$('.nav-bar_menu').children().click(function () {
-    $('.nav-bar_btn').removeClass('active');
-    $('.nav-bar_menu').removeClass('active');
-    $('.btn_icon').removeClass('active');
-});
+    $('.nav-bar_btn').click(function () {
+        $(this).toggleClass('active');
+        $('.nav-bar_menu').toggleClass('active');
+        $('.btn_icon').toggleClass('active');
+    });
+    $('.nav-bar_menu').children().click(function () {
+        $('.nav-bar_btn').removeClass('active');
+        $('.nav-bar_menu').removeClass('active');
+        $('.btn_icon').removeClass('active');
+    });
 
+});
